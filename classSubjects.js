8f47a7499b0430bb3835c13f35cfe26d399b22eb
Your Name <rzhyl05@gmail.com>
2024-11-04 17:38:12 +0800

    your commit message

---
+++ classSubjects.js
@@ -0,0 +1,11 @@
+let fullName = "Rzhyl Greta C. Ramos";
+console.log(fullName);
+
+let age = 19;
+console.log(age);
+
+let favoriteNumber = 24;
+console.log(favoriteNumber);
+
+let favoriteColor = "Red";
+console.log(favoriteColor);
